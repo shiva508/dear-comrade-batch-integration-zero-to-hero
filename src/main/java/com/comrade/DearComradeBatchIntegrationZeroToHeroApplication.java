01ad720e1b4760e4d2d8144fc9f304afff9a6902
shiva508 <dasarishiva1@gmail.com>
2024-05-17 09:10:01 +0530

    first commit

---
+++ src/main/java/com/comrade/DearComradeBatchIntegrationZeroToHeroApplication.java
@@ -0,0 +1,13 @@
+package com.comrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DearComradeBatchIntegrationZeroToHeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DearComradeBatchIntegrationZeroToHeroApplication.class, args);
+	}
+
+}
